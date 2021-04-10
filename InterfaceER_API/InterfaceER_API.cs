222fc6161eed849949f04bd196841a9c1f504e19
Jonathan Alejos Vega <jalejos@dcp.pe>
2021-04-09 20:13:28 -0500

    Add project files.

---
+++ InterfaceER_API/InterfaceER_API.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Text;
+
+namespace InterfaceER_API
+{
+    public class InterfaceER_API
+    {
+        public void IniciarEnvioER()
+        {
+            HttpClient client = new HttpClient();
+            string URI = ConfigurationManager.AppSettings["uriBase"].ToString();
+            client.BaseAddress = new System.Uri(URI);
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            #region USP_LISTA_VENTAS_DIFERENCIAS_CANTIDAD_BDI_OFISIS
+            var VentasDiferenciasCantidadResponse = JsonConvert.DeserializeObject(GetSync($"/api/ListaER?envioCorreo=true", client));
+            #endregion            
+        }
+        private string GetSync(string apiMetodo, HttpClient http)
+        {
+            string retorno = "";
+
+            try
+            {
+                var response = http.GetAsync(apiMetodo).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    retorno = response.Content.ReadAsStringAsync().Result;
+                }
+                else
+                {
+                    retorno = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                return retorno = "Error crítico: " + ex.Message;
+
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ InterfaceER_API/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InterfaceER_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InterfaceER_API interfaceER_API = new InterfaceER_API();
+
+            try
+            {
+                interfaceER_API.IniciarEnvioER();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
